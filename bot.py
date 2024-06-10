1c72a6bee3b1e4d895c9bc2999e50e03750d95e6
niooii <wangh3@bxscience.edu>
2024-06-09 23:44:53 -0400

    working state

--- bot.py
+++ bot.py
@@ -1,17 +1,35 @@
 from threading import Thread
+from typing import Tuple
 
+import numpy as np
 import pyautogui
 from ahk import AHK
 import time
-from enum import Enum
-from pynput.keyboard import Key, Listener
+import cv2 as cv
+from PIL import ImageGrab
+import matplotlib as plt
 
 ahk = AHK()
 ahk.set_coord_mode('Mouse', 'Screen')
 
 
-def get_points_in_rect(top_left: int, bottom_right: int) -> list[(int, int)]:
-    ...
+def get_points_in_rect(top_left: Tuple[int, int], bottom_right: Tuple[int, int]) -> list[(int, int)]:
+    points = []
+
+    x1 = top_left[0]
+    y1 = top_left[1]
+    x2 = bottom_right[0]
+    y2 = bottom_right[1]
+
+    # 5 points per axis
+    points_per_axis: int = 5
+    for i in range(points_per_axis):
+        x = int(x1 + (x2 - x1) * float(i / points_per_axis))
+        for j in range(points_per_axis):
+            y = int(y1 + (y2 - y1) * float(j / points_per_axis))
+            points.append((x, y))
+
+    return points
 
 
 running = True
@@ -21,24 +39,26 @@ class Bot:
 
     @staticmethod
     def click_at(x, y):
-        ahk.mouse_move(x=x, y=y, speed=4, blocking=True)
+        ahk.mouse_move(x=x, y=y, speed=3.5, blocking=True)
         ahk.click(x=x, y=y, blocking=True)
         # whhy not
-        time.sleep(0.05)
+        time.sleep(0.02)
 
     @staticmethod
-    def img_exists_on_screen(img_path: str, confidence: float):
+    def img_exists_on_screen(img_path: str, confidence: float, region=None) -> bool:
         try:
-            box = pyautogui.locateOnScreen(img_path, confidence=confidence)
+            box = pyautogui.locateOnScreen(img_path, confidence=confidence, region=region)
+            pyautogui.locateOnScreen(img_path, confidence=confidence)
         except pyautogui.ImageNotFoundException as e:
             return False
 
         return box is not None
 
     @staticmethod
-    def try_click_img(img_path: str, confidence: float, search_time: float):
+    def try_click_img(img_path: str, confidence: float, search_time: float = 0, region=None) -> bool:
         try:
-            box = pyautogui.locateOnScreen(img_path, confidence=confidence, minSearchTime=search_time)
+            box = pyautogui.locateOnScreen(img_path, confidence=confidence, minSearchTime=search_time, region=region)
+
             x = box.left + box.width / 2
             y = box.top + box.height / 2
             Bot.click_at(x, y)
@@ -47,16 +67,71 @@ class Bot:
         return True
 
     @staticmethod
-    # THIS ONLY WORKS PERFECTLY FOR 15FPS LOL cap t hat shit
+    def get_img_coords(img_path: str, confidence: float, search_time: float, region=None) -> Tuple[int, int] | None:
+        try:
+            box = pyautogui.locateOnScreen(img_path, confidence=confidence, minSearchTime=search_time, region=region)
+
+            x = box.left + box.width / 2
+            y = box.top + box.height / 2
+            return int(x), int(y)
+        except pyautogui.ImageNotFoundException as e:
+            return None
+
+    @staticmethod
+    def try_click_img_opencv(img_path: str, confidence: float, region=None):
+        haystack = np.array(ImageGrab.grab())
+        needle = cv.imread(img_path, cv.IMREAD_UNCHANGED)
+        needle_w, needle_h = needle.shape[1], needle.shape[0]
+
+        # run the OpenCV algorithm
+        result = cv.matchTemplate(haystack, needle, cv.TM_CCOEFF_NORMED)
+        # print("1--- %s seconds ---1" % (time.time() - start_time))
+        # Get the positions from the match result that exceed our threshold
+        locs = np.column_stack(np.where(result >= 0.5))
+
+        # Group rectangles to eliminate redundancy
+        rectangles = [(x, y, needle_w, needle_h) for x, y in locs]
+        rectangles, _ = cv.groupRectangles(rectangles, groupThreshold=1, eps=0.5)
+
+        points = []
+        print(f"found {len(rectangles)} tangles.")
+        # cv.imshow('cv result', haystack)
+        # cv.waitKey(500)
+        if rectangles is None or len(rectangles) == 0:
+            return False
+        line_color = (0, 255, 0)
+        line_type = cv.LINE_4
+
+        # # Loop over all the rectangles
+        for (y, x, w, h) in rectangles:
+            #     # Determine the center position
+            center_x, center_y = x + int(w / 2), y + int(h / 2)
+            #     # Save the points
+            points.append((center_x, center_y))
+
+            # Determine the box position
+            top_left, bottom_right = (x, y), (x + w, y + h)
+            # Draw the box
+            cv.rectangle(haystack, top_left, bottom_right, color=line_color, lineType=line_type, thickness=2)
+
+        x, y = points[0]
+        Bot.click_at(x, y)
+
+        return True
+
+    @staticmethod
+    # THIS ONLY WORKS PERFECTLY FOR 30FPS LOL cap t hat shit
     def get_to_playarea():
         keydelay: int = 80
-        ahk.send('s', key_press_duration=2000, key_delay=keydelay)
+        ahk.send('s', key_press_duration=1000, key_delay=keydelay)
         ahk.send('a', key_press_duration=2500, key_delay=keydelay)
         ahk.send('s', key_press_duration=1250, key_delay=keydelay)
         # start going forward from the corner
         ahk.send('w', key_press_duration=3250, key_delay=keydelay)
-        ahk.send('a', key_press_duration=330, key_delay=keydelay)
-        ahk.send('w', key_press_duration=2500, key_delay=keydelay)
+        ahk.send('a', key_press_duration=700, key_delay=keydelay)
+        ahk.send('w', key_press_duration=800, key_delay=keydelay)
+        ahk.send('d', key_press_duration=210, key_delay=keydelay)
+        ahk.send('w', key_press_duration=2275, key_delay=keydelay)
         ahk.send('a', key_press_duration=350, key_delay=keydelay)
         ahk.send('w', key_press_duration=925, key_delay=keydelay)
         ahk.send('a', key_press_duration=350, key_delay=keydelay)
@@ -75,17 +150,30 @@ class Bot:
 
     @staticmethod
     # top_left and bottom_right define a rectangular region to interpolate over and try placing down units
-    def place_units(top_left: int, bottom_right: int):
+    def place_units(top_left: Tuple[int, int], bottom_right: Tuple[int, int]):
         points = get_points_in_rect(top_left, bottom_right)
         for (x, y) in points:
-            ...
-        ...
+            ahk.send('1')
+            time.sleep(0.05)
+            ahk.mouse_move(x, y, speed=3)
+            ahk.click(x, y)
+            time.sleep(0.05)
+            ahk.send('c')
 
     @staticmethod
     # top_left and bottom_right should be the same as the ones passed into place_units
-    def upgrade_units(top_left: int, bottom_right: int):
+    # UPGRADE SCREEN MUST BE ON LEFT SIDE
+    def upgrade_units(top_left: Tuple[int, int], bottom_right: Tuple[int, int], client_x: int, client_y: int,
+                      client_w: int, client_h: int):
         points = get_points_in_rect(top_left, bottom_right)
         for (x, y) in points:
             # click, then if an image is found with the upgrade button click it a few times and continue
-            ...
-        ...
+            ahk.mouse_move(x, y, speed=3)
+            ahk.click(x, y)
+            time.sleep(0.2)
+            while Bot.try_click_img('upgrade_button.jpg', confidence=0.99,
+                                 region=(client_x, client_y, int(client_w/2), client_h)):
+                print("Upgraded unit!")
+                # clear the view for bot to detect img again
+                coords = ahk.get_mouse_position()
+                ahk.mouse_move(coords.x, coords.y + 125, relative=True, speed=5)
--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import numpy as np
 import os
 from time import time
 
+import win32gui
 from pynput.keyboard import Listener, Key
 
 from bot import Bot
-from windowcapture import WindowCapture
 import pyautogui
 from threading import Thread
 import time
@@ -51,36 +51,53 @@ bot = Bot()
 
 class GameState(Enum):
     IN_LOBBY = 1
-    PLACING_UNITS = 2
-    UPGRADING_UNITS = 3
-    TELEPORTING_TO_INF = 4
+    TELEPORTING_TO_INF = 2
+    PLACING_UNITS = 3
+    UPGRADING_UNITS = 4
+    INF_FAILED = 5
+    TELEPORTING_FROM_INF = 6
 
 
 gamestate = GameState.IN_LOBBY
 
-# list of where to click to activate window focus. make configurable
-centers = [
-    (612, 350),
-    (612, 930),
-    (1923, 966)
-]
+# a list of each window's (x, y, w, h)
+rblx_windows = []
+
+
+# get properties of all roblox windows
+def callback(hwnd, extra):
+    rect = win32gui.GetWindowRect(hwnd)
+    x = rect[0]
+    y = rect[1]
+    x = x if x > 0 else 0
+    y = y if y > 0 else 0
+    w = rect[2] - x
+    h = rect[3] - y
+    window_title: str = win32gui.GetWindowText(hwnd)
+    if window_title == 'Roblox':
+        rblx_windows.append((x, y, w, h))
+        print("Window: %s" % window_title)
+        print("\tLocation: (%d, %d)" % (x, y))
+        print("\t    Size: (%d, %d)" % (w, h))
+
+
+win32gui.EnumWindows(callback, None)
 
 
 def switch_client(idx: int):
-    x, y = centers[idx]
-    bot.click_at(x, y)
+    x, y, w, h = rblx_windows[idx]
+    bot.click_at(x + w / 2, y + h / 2)
 
 
 def for_each_rblx(func):
-    for x, y in centers:
-        bot.click_at(x, y)
-        func()
+    for x, y, w, h in rblx_windows:
+        bot.click_at(x + w / 2, y + h / 2)
+        func(x, y, w, h)
         if not running:
             return
 
 
 stage = 0
-
 # while True:
 #     if not bot.try_click_img('windmill_village_button.jpg', 0.8):
 #         print("failed to find img")
@@ -90,13 +107,15 @@ while running:
     match gamestate:
         case GameState.IN_LOBBY:
             if stage == 0:
-                for_each_rblx(bot.get_to_playarea)
+                for_each_rblx(lambda x, y, w, h: bot.get_to_playarea())
                 stage = 1
 
             if stage == 1:
-                # first client selects world rest join it
-                for i, (x, y) in enumerate(centers):
-                    bot.click_at(x, y)
+                # first client selects world rest join itwdw
+                for i, (x, y, w, h) in enumerate(rblx_windows):
+                    if not running:
+                        break
+                    bot.click_at(x + w / 2, y + h / 2)
                     bot.enter_portal()
                     if i == 0:
                         # select world and shit
@@ -115,22 +134,134 @@ while running:
                 if not bot.try_click_img('start_inf.jpg', 0.8, 2):
                     print("could not find 'start' button")
                     continue
+                print("Teleporting to infinite...")
                 gamestate = GameState.TELEPORTING_TO_INF
                 continue
 
         case GameState.TELEPORTING_TO_INF:
             # look for sign that inf has started
-            if bot.img_exists_on_screen('skip_wave.jpg', confidence=0.85):
-                print("inf has started chat")
-                gamestate = GameState.PLACING_UNITS
+            if stage == 0:
+                if bot.img_exists_on_screen('skip_wave.jpg', confidence=0.85):
+                    print("inf has started chat")
+                    # wait until the notif goes away i guess
+                    time.sleep(5)
+                    stage = 1
+
+            # look for it again to confirm wave 2 finished, we need money
+            if stage == 1:
+                if bot.img_exists_on_screen('skip_wave.jpg', confidence=0.85):
+                    print("wave 2 started, waiting 3.5 seconds...")
+                    gamestate = GameState.PLACING_UNITS
+                    time.sleep(3.5)
+                    stage = 0
 
         case GameState.PLACING_UNITS:
-            ahk.mouse_move(500, 500)
-            time.sleep(2)
-            # try placing units on all clients
-            ...
+            # second time the skip wave thing pops up is when we got enough cash for a mythic
+            print("placing units...")
+
+
+            def place(x, y, w, h):
+                global gamestate
+                # just return if failure detected
+                if gamestate != GameState.PLACING_UNITS:
+                    return
+                if bot.img_exists_on_screen('infinite_end_screen.jpg', confidence=0.85):
+                    gamestate = GameState.INF_FAILED
+                    print("Infinite failed, leaving game")
+                    return
+                top_left = (x + w * 0.7, y + h * 0.5)
+                bot_right = (x + w * 0.95, y + h * 0.95)
+                bot.place_units(top_left, bot_right)
+
+
+            for_each_rblx(place)
+            gamestate = GameState.UPGRADING_UNITS
+            print("upgrading units...")
+            stage = 0
 
         case GameState.UPGRADING_UNITS:
             # try placing units on all clients
             # this runs until the big red return button is spotted
-            ...
+            for x, y, w, h in rblx_windows:
+                if not running:
+                    break
+                if bot.img_exists_on_screen('infinite_end_screen.jpg', confidence=0.85):
+                    gamestate = GameState.INF_FAILED
+                    print("Infinite failed, leaving game")
+                    break
+                bot.click_at(x + w / 2, y + h / 2)
+
+                top_left = (x + w * 0.7, y + h * 0.5)
+                bot_right = (x + w * 0.95, y + h * 0.95)
+                bot.upgrade_units(top_left, bot_right, x, y, w, h)
+            stage += 1
+            if stage >= 4:
+                stage = 0
+                gamestate = GameState.PLACING_UNITS
+
+        case GameState.INF_FAILED:
+            # a hacky workaround because image detection just does not want to work for all 4 clients
+            found_return = False
+            rel_shift_x = -1
+            rel_shift_y = -1
+
+
+            def find_return(x, y, w, h):
+                global found_return, rel_shift_x, rel_shift_y
+                if found_return:
+                    return
+                # print(x)
+                # print(y)
+                # print(w)
+                # print(h)
+                result = bot.get_img_coords('return_lobby_button.jpg', confidence=0.75, region=(x, y, w, h),
+                                            search_time=2)
+                if result is not None:
+                    found_return = True
+                    _x = result[0]
+                    _y = result[1]
+                    rel_shift_x = _x - x
+                    rel_shift_y = _y - y
+                    return
+
+
+            # goes through each roblox window and tries to find return button
+            for_each_rblx(find_return)
+            if not found_return:
+                print("Could not find return button")
+                continue
+
+
+            def click_return(x, y, w, h):
+                global rel_shift_x, rel_shift_y
+                bot.click_at(x + rel_shift_x, y + rel_shift_y)
+
+
+            for_each_rblx(click_return)
+
+            gamestate = GameState.TELEPORTING_FROM_INF
+            print("Returning to lobby...")
+
+        case GameState.TELEPORTING_FROM_INF:
+            # do a check to see if all clients have the shop button on screen
+            ready_to_restart = True
+
+
+            def check_if_ready(x, y, w, h):
+                global ready_to_restart
+                if not ready_to_restart:
+                    return
+
+                if not bot.img_exists_on_screen('shop.jpg', confidence=0.8, region=(x, y, w, h)):
+                    ready_to_restart = False
+
+
+            for_each_rblx(check_if_ready)
+
+            if not ready_to_restart:
+                print("Waiting for all clients to be in lobby...")
+                continue
+
+            gamestate = GameState.IN_LOBBY
+            stage = 0
+            print("Return to lobby successful.")
